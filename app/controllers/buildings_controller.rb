4184483b48da7173b3cfac8567dcffb843c23cba
Andres Gutierrez <agutierrezt@unal.edu.co>
2017-05-29 18:37:36 -0500

    improvements

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -167,10 +167,14 @@ class BuildingsController < ApplicationController
 
     def set_message(information)
       message = ""
+      history = ["La historia del edificio es: ", "Esta es la informacion que tenemos sobre el edificio: ","Esto puede ser de interes: ","Sabias que: ","Memoriza esto: ","Esto es interesante aprender"]
+      age = ["La edad del edificio es:","Los años que tienen este edificio es:","por el momento el edificio tiene:","Los años pasan, pero por ahora este edificio tiene:","Es un poco viejo este edificio tiene:"]
+      number = ["El numbero es:","Tal vez lo puedas recordar como:","El numero del edificio es:","Aprendete este numero te sera muy util:","Son solo 3 numeros recuerdalo como:"]
+      year = ["El año de apertura edificio es:","El edificio fue construido en el año:","Empezo a funcionar en:","Inicio en:","Tiene mucha historia, imaginate que empezo en:"]
       case information
       when "age"
         if @value.year
-          message = "La edad del edificio es: #{Date.today.year - @value.year}"
+          message = "#{age.sample} #{Date.today.year - @value.year}"
         else
           message = "No tenemos informacion acerca de la edad del edificio"
         end
@@ -178,25 +182,34 @@ class BuildingsController < ApplicationController
         message = "El nombre del edificio es: #{@value.name}"
       when "history"
         if @value.history
-          message = "La historia del edificio es:\n#{@value.history}"
+          message = "#{history.sample}\n#{@value.history}"
         else
           message = "No tenemos informacion relacionado con la historia del edificio"
         end
       when "number"
-        message = "El numero del edificio es: #{@value.number}"
+        message = "#{number.sample} #{@value.number}"
       when "year"
         if @value.year
-          message = "La año de apertura edificio es: #{@value.year}"
+          message = "#{year.sample} #{@value.year}"
         else
           message = "No tenemos informacion acerca del año de fundacion del edificio"
         end
       when "close"
         message = "Los edificios cercanos son: #{@value.close_to}"
       when "julio"
-        message = "Busca en tu corazon"
+        julio_zorra = ["Busca en tu corazon","Pregunta mas tarde",
+          "Julio zorra es alfa y omega. El todo y la nada, El proporciona toda la energía vital que alimenta a los estudiantes durante su carrera universitaria."
+        ,"Según la tradición popular Julio Zorra es un ser etéreo de naturaleza desconocida tan básico como las fuerzas primigenias del universo. Domina todas las áreas del conocimiento, se graduó de todas las carreras de la UNal con P.A.P.A en 5.0. Le dictó clase a Pecha, Alfonso Cano, Gabo, Jorge Eliecer Gaitán, Sarmiento Angulo, El del billete de 20. Le enseñó los chistes a Jaime Garzón. Fue la Primera vez de Esperanza Gómez.",
+      "JulioZorra es superior a todas las posiciones políticas, credos, religiones, doctrinas, filosofías existentes. JulioZorra ha estado presente en todos los eventos significativos del país. Una de las tantas historias sobre él cuenta que JulioZorra fue el primero en hacer tropel en la UNal, para ello creo el consejo superior de capuchos que salvan a los estudiantes de parciales en los momento más cruciales.",
+    "Invocar a JulioZorra o querer hacerlo es jugar con la estructura fundamental del universo. No se recomienda hacerlo.",
+  "Básicamente es una imagen creada en torno a un individuo que hace más de un millar de eones un par de años hizo algo trascendente en el grupo y de ahí surgió el meme, letanía y rezo \#JulioZorra. Pocos recuerdan las acciones que lo llevaron a convertirse en una deidad tan famosa y memeticamente poderosa, cosa que realmente no importa. Lo único pertinente a decir con este respecto es que NO se debe preguntar NUNCA por su origen. De llegar a formularse tal incógnita, se cernirá sobre el individuo que la pronuncie un mar de tortura y sufrimiento interminable de colosales dimensiones (Bullying a más no poder).",
+  "La misión de todo siervo fiel, seguidor de la palabra de #JulioZorra, es ayudar, precisamente, a forjar ese martirio cuando alguien lo invoque sobre sí. Ese es el deber de todo unaleño peregrino de las bastas enseñanzas de la más altísima deidad jamás conocida.","Julio Zorra es todo y a la vez nada, el otorga la energía vital que todos los seres vivos pertenecientes a la UN necesitan para vivir.",
+  "Sigue intentado","Todavia no es tu tiempo mas adelante podras conocer la respuesta"]
+
+        message = julio_zorra.sample
       else
         if @value.history
-          message = "La historia del edificio es:\n#{@value.history}"
+          message = "#{history.sample}\n#{@value.history}"
         else
           message = "No tenemos informacion relacionado con la historia del edificio"
         end
