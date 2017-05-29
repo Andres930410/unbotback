48388fc8dec41f6f8be050bfa3f245b350ce75fe
Andres Gutierrez <agutierrezt@unal.edu.co>
2017-05-29 13:06:56 -0500

    small changes

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -38,6 +38,10 @@ class BuildingsController < ApplicationController
     @building.destroy
   end
 
+  def location
+    
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_building
