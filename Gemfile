90bb23bb7f980185f837cf6942ea84202c8a6cd2
Andres Gutierrez <agutierrezt@unal.edu.co>
2017-05-29 14:17:14 -0500

    fog

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'puma', '~> 3.0'
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
 gem 'carrierwave', '~> 1.0'
+gem 'fog'
 
 
 group :development, :test do
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,13 +5,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
   # storage :fog
-
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
 
   def extension_whitelist
     %w(jpg jpeg gif png)
